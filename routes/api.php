2ff295747f7c6ce485017cf89cf6b66aeaf9348a
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-07-06 01:52:28 -0300

    feat: modificado middlewares para uso de ability

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ProductController;
 
 /* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -14,24 +16,20 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
-/* Route::prefix('posts')->group(function () {
-    Route::get('/', [PostController::class, 'index']);
-    Route::post('/', [PostController::class, 'store']);
-    Route::get('/{id}', [PostController::class, 'show']);
-    Route::put('/{id}', [PostController::class,  'update']);
-    Route::delete('/{id}', [PostController::class, 'destroy']);
-}); */
-
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{post}', [PostController::class, 'show']);
-/* Route::middleware('auth:sanctum')->group(function () {
-    // Rotas protegidas que exigem autenticação
-    Route::post('/posts', [PostController::class, 'store']);
-    Route::put('/posts/{post}', [PostController::class, 'update']);
-    Route::delete('/posts/{post}', [PostController::class, 'destroy']);
-}); */
-
-Route::apiResource('posts', PostController::class)
+Route::middleware('auth:sanctum')->group(function() {
+    Route::apiResource('posts', PostController::class)
     ->parameters([
         'posts' => 'post'
-    ])->middleware('auth:sanctum');
+    ])->middleware('ability:user,admin');
+
+    Route::apiResource('users', UserController::class)
+        ->parameters([
+            'users' => 'user'
+        ])->middleware('ability:admin');
+
+    Route::apiResource('products', ProductController::class)
+        ->parameters([
+            'products' => 'product'
+        ])->middleware('ability:user,manager,admin');
+});
+
